b516344264c852d7361b36f54a9106bf8dfd1505
Ásgeir Bjarnason <asgeir.bjarnason@reykjavik.is>
2012-11-02 01:05:31 +0000

    Uppfærði serial_print

--- test_forrit/serial_print/serial_print.ino
+++ test_forrit/serial_print/serial_print.ino
@@ -1,18 +1,9 @@
-
-//boolean clicked = false;
-const int buttonpin = 12;
-
-int buttonstate = 0;
-
 void setup() {
   pinMode(buttonpin, INPUT); 
+  Serial.begin(9600);
 }
 
 void loop() {
-  buttonstate = digitalRead(buttonpin);
-  
-  if (buttonstate == HIGH) {
-    Serial.println("Button pressed");
-  }
+  Serial.println("Just a test");
+  delay(1000);
 }
-
